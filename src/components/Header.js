40f3709bb220099f9342ca74c5a4565e0fdc48f5
agdamaciel-22 <agdamyrella12@hotmail.com>
2022-07-14 23:05:38 -0300

    Requisito 8/9

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import propTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title, hasSearch }) {
+  const history = useHistory();
+  const [barVisible, setBarVisible] = useState(false);
+
+  function openBar() {
+    setBarVisible(!barVisible);
+  }
+
   return (
     <>
-      <img data-testid="profile-top-btn" src={ profileIcon } alt="Profile Icon" />
-      {hasSearch
-      && <img data-testid="search-top-btn" src={ searchIcon } alt="Search Icon" />}
+      <button type="button" onClick={ () => history.push('/profile') }>
+        <img
+          src={ profileIcon }
+          data-testid="profile-top-btn"
+          alt="Profile icon"
+        />
+      </button>
+      {
+        hasSearch
+        && (
+          <button type="button" onClick={ openBar }>
+            <img
+              src={ searchIcon }
+              data-testid="search-top-btn"
+              alt="Search Icon"
+            />
+          </button>
+        )
+      }
+      {
+        barVisible
+        && <input data-testid="search-input" type="text" />
+      }
       <h1 data-testid="page-title">{title}</h1>
     </>
   );
