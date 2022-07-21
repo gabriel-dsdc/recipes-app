534fbee88a2006707391962728e99c5736ec7378
Mayara A. Moraes <salanderaraujo@gmail.com>
2022-07-21 18:44:45 -0300

    Adiciona requisitos 25 e 26

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import MyContext from '../context/MyContext';
 import { fetchRecipeWithID } from '../services/api';
+
+const MAX_LENGTH = 6;
 
 function RecipeDetails() {
   const { location: { pathname } } = useHistory();
@@ -8,16 +11,33 @@ function RecipeDetails() {
   const path = splited[1];
   const id = splited[2];
   const [currentRecipe, setCurrentRecipe] = useState([]);
-  const type = path === 'foods' ? 'Meal' : 'Drink';
+  const [ingList, setIngList] = useState([]);
+  const [measureList, setmeasureList] = useState([]);
+  const [recommend, setRecommend] = useState([]);
+  const type = (path === 'foods') ? 'Meal' : 'Drink';
+  const { defaultFood, defaultDrinks } = useContext(MyContext);
 
   useEffect(() => {
     async function getRecipe() {
       const recipe = await fetchRecipeWithID(path, id);
       setCurrentRecipe(recipe);
-      setObjRecipe(Object.entries(recipe[0]));
+      const ingredientList = Object.entries(recipe[0]).filter((entry) => entry[0]
+        .includes('strIngredient'))
+        .filter((entry) => entry[1] !== '' && entry[1] !== null);
+      setIngList(ingredientList);
+      const measList = Object.entries(recipe[0]).filter((entry) => entry[0]
+        .includes('strMeasure'))
+        .filter((entry) => entry[1] !== null && entry[1] !== ' ');
+      setmeasureList(measList);
     }
     getRecipe();
   }, []);
+
+  useEffect(() => {
+    const defaultResults = path.includes('foods') ? defaultFood : defaultDrinks;
+    const renderResults = defaultResults.slice(0, MAX_LENGTH);
+    setRecommend(renderResults);
+  }, [defaultFood, defaultDrinks, id, path]);
 
   const recipe = currentRecipe[0];
   return (
@@ -49,6 +69,15 @@ function RecipeDetails() {
               )
             }
             <h3>Ingredientes</h3>
+            { ingList[0] && measureList[0]
+            && ingList.map((ing, index) => (
+              <p key={ index } data-testid={ `${index}-ingredient-name-and-measure` }>
+                {ing[1]}
+                :
+                {' '}
+                {measureList[index][1]}
+              </p>
+            ))}
             <h3>Instruções</h3>
             <p data-testid="instructions">{ currentRecipe[0].strInstructions}</p>
             {
@@ -65,6 +94,20 @@ function RecipeDetails() {
               )
             }
             <h3>Recomendações</h3>
+            {
+              recommend.map((recomendation, index) => (
+                <button
+                  type="button"
+                  key={ index }
+                  data-testid={ `${index}-recomendation-card` }
+                >
+                  <img src={ recomendation[`str${type}Thumb`] } alt="thumb recommend" />
+                  <p data-testid={ `${index}-recomendation-title` }>
+                    { recomendation[`str${type}`] }
+                  </p>
+                </button>
+              ))
+            }
           </div>
         )
       }
