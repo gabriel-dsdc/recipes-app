d9aa1d71c5a0a31bb813dad659d6b6531868310b
agdamaciel-22 <agdamyrella12@hotmail.com>
2022-07-22 11:54:31 -0300

    corrigindo quebra

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
@@ -52,11 +51,5 @@ function Profile() {
     </>
   );
 }
-
-Profile.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func,
-  }).isRequired,
-};
 
 export default Profile;
