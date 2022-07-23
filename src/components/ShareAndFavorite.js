f938948bc76f71789b6f7f53348bd9e0de498bf5
Mayara A. Moraes <salanderaraujo@gmail.com>
2022-07-23 19:23:46 -0300

    Adiciona requisito 36 e novo componente com os botão de compartilhar e favoritar

---
+++ src/components/ShareAndFavorite.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from 'react';
+import propTypes from 'prop-types';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import shareIcon from '../images/shareIcon.svg';
+
+const copy = require('clipboard-copy');
+
+function ShareAndFavorite({ id, shareMessage, recipe, type }) {
+  const INITAL_COMPONENT_STATE = {
+    shareMessage,
+    recipe,
+    source: recipe.strSource,
+    id,
+    favoriteIcon: whiteHeartIcon,
+  };
+  const [shareFavoriteState, setShareFavorite] = useState(INITAL_COMPONENT_STATE);
+
+  function isFavorite() {
+    const savedRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    if (savedRecipes.some((recip) => recip.id === shareFavoriteState.id)) {
+      setShareFavorite((prevState) => ({
+        ...prevState,
+        favoriteIcon: blackHeartIcon,
+      }));
+    }
+  }
+
+  useEffect(() => {
+    function getFavorites() {
+      if (localStorage.getItem('favoriteRecipes')) {
+        isFavorite();
+      }
+    }
+    getFavorites();
+  }, []);
+
+  function toggleFavorites(fav) {
+    const savedRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    if (savedRecipes.some((item) => item.id === id)) {
+      const newFav = savedRecipes.filter((item) => item.id !== id);
+      localStorage.setItem('favoriteRecipes', JSON.stringify((newFav)));
+      setShareFavorite((prevState) => ({
+        ...prevState,
+        favoriteIcon: whiteHeartIcon,
+      }));
+    } else {
+      const array = [...savedRecipes, ...fav];
+      localStorage.setItem('favoriteRecipes', JSON.stringify((array)));
+      setShareFavorite((prevState) => ({
+        ...prevState,
+        favoriteIcon: blackHeartIcon,
+      }));
+    }
+  }
+
+  function firstFavorite(favs) {
+    localStorage.setItem('favoriteRecipes', JSON.stringify((favs)));
+    setShareFavorite((prevState) => ({
+      ...prevState,
+      favoriteIcon: blackHeartIcon,
+    }));
+  }
+
+  function setFavorites(favoriteRecipe) {
+    const favorites = [{
+      id,
+      type: type === 'Meal' ? 'food' : 'drink',
+      nationality: type === 'Meal' ? favoriteRecipe.strArea : '',
+      category: favoriteRecipe.strCategory,
+      alcoholicOrNot: type === 'Meal' ? '' : favoriteRecipe.strAlcoholic,
+      name: favoriteRecipe[`str${type}`],
+      image: favoriteRecipe[`str${type}Thumb`],
+    }];
+    if (!localStorage.getItem('favoriteRecipes')) {
+      firstFavorite(favorites);
+    } else {
+      toggleFavorites(favorites);
+    }
+  }
+
+  return (
+    <section>
+      {
+        shareFavoriteState.shareMessage ? (<p>Link copied!</p>) : (
+          <input
+            type="image"
+            onClick={ () => {
+              copy(shareFavoriteState.source);
+              setShareFavorite((prevState) => ({
+                ...prevState,
+                shareMessage: true,
+              }));
+            } }
+            src={ shareIcon }
+            data-testid="share-btn"
+            alt="shareicon"
+          />
+        )
+      }
+      <input
+        type="image"
+        onClick={ () => { setFavorites(recipe); } }
+        src={ shareFavoriteState.favoriteIcon }
+        data-testid="favorite-btn"
+        alt="favoriteicon"
+      />
+    </section>
+  );
+}
+
+ShareAndFavorite.propTypes = {
+  shareMessage: propTypes.bool.isRequired,
+  recipe: propTypes.objectOf(propTypes.string).isRequired,
+  type: propTypes.string.isRequired,
+  id: propTypes.string.isRequired,
+};
+
+export default ShareAndFavorite;
--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -2,12 +2,9 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import MyContext from '../context/MyContext';
 import { fetchRecipeWithID } from '../services/api';
-import shareIcon from '../images/shareIcon.svg';
 import mapIngredients from '../services/mapIngredients';
-import whiteHeartIcon from '../images/whiteHeartIcon.svg';
-import blackHeartIcon from '../images/blackHeartIcon.svg';
-
-const copy = require('clipboard-copy');
+import styles from '../services/styles';
+import ShareAndFavorite from '../components/ShareAndFavorite';
 
 const MAX_LENGTH = 6;
 
@@ -20,167 +17,111 @@ function RecipeDetails() {
   const path = splited[1];
   const id = splited[2];
 
-  const [currentRecipe, setCurrentRecipe] = useState([]);
-  const [ingList, setIngList] = useState([]);
-  const [measureList, setmeasureList] = useState([]);
-  const [recommend, setRecommend] = useState([]);
-  const typeRecomendation = (path === 'foods') ? 'Drink' : 'Meal';
-  const type = (path === 'foods') ? 'Meal' : 'Drink';
-  const [objRecipe, setObjRecipe] = useState([]);
-  const [shareMessage, setShareMessage] = useState(false);
-  const [favoriteIcon, setFavoriteIcon] = useState(whiteHeartIcon);
+  const DETAILS_RECIPE_STATE = {
+    currentRecipe: [],
+    ingList: [],
+    measureList: [],
+    recommend: [],
+    typeRecomendation: (path === 'foods') ? 'Drink' : 'Meal',
+    type: (path === 'foods') ? 'Meal' : 'Drink',
+    objRecipe: [],
+    shareMessage: false,
+  };
+
+  const [state, setState] = useState(DETAILS_RECIPE_STATE);
 
   useEffect(() => {
     async function getRecipe() {
       const recipe = await fetchRecipeWithID(path, id);
-      setCurrentRecipe(recipe);
-      setObjRecipe(recipe[0]);
-      setIngList(mapIngredients(recipe[0], 'strIngredient'));
-      setmeasureList(mapIngredients(recipe[0], 'strMeasure'));
+      const ingredientes = (mapIngredients(recipe[0], 'strIngredient'));
+      const measures = (mapIngredients(recipe[0], 'strMeasure'));
+      setState((prevState) => ({
+        ...prevState,
+        currentRecipe: recipe,
+        objRecipe: recipe[0],
+        ingList: ingredientes,
+        measureList: measures,
+      }));
     }
     getRecipe();
   }, []);
 
   useEffect(() => {
-    function getFavorites() {
-      if (localStorage.getItem('favoriteRecipes')) {
-        const savedRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
-        if (savedRecipes.some((recip) => recip.id === id)) {
-          setFavoriteIcon(blackHeartIcon);
-        }
-      }
-    }
-    getFavorites();
-  }, []);
-
-  useEffect(() => {
     const defaultResults = path.includes('foods') ? defaultDrinks : defaultFood;
     const renderResults = defaultResults.slice(0, MAX_LENGTH);
-    setRecommend(renderResults);
+    setState((prevState) => ({
+      ...prevState,
+      recommend: renderResults,
+    }));
   }, [defaultFood, defaultDrinks, id, path]);
 
-  const styles = {
-    divButton: {
-      position: 'fixed',
-      bottom: 0,
-    },
-    firstImg: {
-      width: '200px',
-    },
-    mainDiv: {
-      width: '300px',
-      display: 'flex',
-      overflow: 'scroll',
-    },
-    subDivs: {
-      width: '50vw',
-    },
-    subDivsImg: {
-      width: '150px',
-    },
-  };
-
   function handleStartRecipe() {
-    setShareMessage(false);
+    setState((prevState) => ({
+      ...prevState,
+      shareMessage: false,
+    }));
     history.push(`${pathname}/in-progress`);
-  }
-
-  function favoriteRecipe(recipe) {
-    setFavoriteIcon(blackHeartIcon);
-    const favorites = [
-      {
-        id,
-        type: type === 'Meal' ? 'food' : 'drink',
-        nationality: type === 'Meal' ? recipe.strArea : '',
-        category: recipe.strCategory,
-        alcoholicOrNot: type === 'Meal' ? '' : recipe.strAlcoholic,
-        name: recipe[`str${type}`],
-        image: recipe[`str${type}Thumb`],
-      },
-    ];
-    if (!localStorage.getItem('favoriteRecipes')) {
-      localStorage.setItem('favoriteRecipes', JSON.stringify((favorites)));
-    } else {
-      const savedRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
-      const array = [...savedRecipes, ...favorites];
-      console.log(array);
-      localStorage.setItem('favoriteRecipes', JSON.stringify((array)));
-    }
   }
 
   return (
     <div>
       {
-        currentRecipe && (
+        state.currentRecipe && (
           <div>
             <div>
               <img
-                src={ objRecipe[`str${type}Thumb`] }
+                src={ state.objRecipe[`str${state.type}Thumb`] }
                 style={ styles.firstImg }
                 data-testid="recipe-photo"
                 alt="recipe img"
               />
               <h2 data-testid="recipe-title">
                 {' '}
-                { objRecipe[`str${type}`] }
+                { state.objRecipe[`str${state.type}`] }
                 {' '}
               </h2>
-              <button
-                data-testid="share-btn"
-                type="button"
-                onClick={ () => { copy(objRecipe.strSource); setShareMessage(true); } }
-              >
-                {
-                  shareMessage ? (<p>Link copied!</p>) : (
-                    <img
-                      src={ shareIcon }
-                      alt="shareIcon"
-                    />
-                  )
-                }
-              </button>
-              <input
-                type="image"
-                onClick={ () => { favoriteRecipe(objRecipe); } }
-                src={ favoriteIcon }
-                data-testid="favorite-btn"
-                alt="favoriteicon"
-              />
-
+              {
+                state.currentRecipe[0]
+                && <ShareAndFavorite
+                  recipe={ state.objRecipe }
+                  shareMessage={ state.shareMessage }
+                  type={ state.type }
+                  id={ id }
+                />
+              }
             </div>
-
             {
               path === 'foods' ? (
                 <p data-testid="recipe-category">
                   {' '}
-                  { objRecipe.strCategory }
+                  { state.objRecipe.strCategory }
                 </p>
               ) : (
                 <p data-testid="recipe-category">
                   {' '}
-                  { objRecipe.strAlcoholic }
+                  { state.objRecipe.strAlcoholic }
                 </p>
               )
             }
             <h3>Ingredientes</h3>
-            { ingList[0] && measureList[0]
-            && ingList.map((ing, index) => (
+            { state.ingList[0] && state.measureList[0]
+            && state.ingList.map((ing, index) => (
               <p key={ index } data-testid={ `${index}-ingredient-name-and-measure` }>
                 {ing[1]}
                 :
                 {' '}
-                {measureList[index][1]}
+                {state.measureList[index][1]}
               </p>
             ))}
             <h3>Instruções</h3>
-            <p data-testid="instructions">{ objRecipe.strInstructions}</p>
+            <p data-testid="instructions">{ state.objRecipe.strInstructions}</p>
             {
               path === 'foods' && (
 
                 <div>
                   <p>Vídeo</p>
                   <iframe
-                    src={ objRecipe.strYoutube }
+                    src={ state.objRecipe.strYoutube }
                     title="Youtube video play"
                   />
                   <p data-testid="video" />
@@ -190,7 +131,7 @@ function RecipeDetails() {
             <h3>Recomendações</h3>
             <div style={ styles.mainDiv }>
               {
-                recommend.map((recomendation, index) => (
+                state.recommend.map((recomendation, index) => (
                   <button
                     style={ styles.subDivs }
                     type="button"
@@ -199,11 +140,11 @@ function RecipeDetails() {
                   >
                     <img
                       style={ styles.subDivsImg }
-                      src={ recomendation[`str${typeRecomendation}Thumb`] }
+                      src={ recomendation[`str${state.typeRecomendation}Thumb`] }
                       alt="thumb recommend"
                     />
                     <p data-testid={ `${index}-recomendation-title` }>
-                      { recomendation[`str${typeRecomendation}`] }
+                      { recomendation[`str${state.typeRecomendation}`] }
                     </p>
                   </button>
                 ))
