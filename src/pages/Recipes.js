1778d495ce6d2363eb9236375ed56347d22a23b9
Alfeu Kramer <alfeukramer@gmail.com>
2022-07-25 17:24:53 -0300

    Ajustes botão categoria

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,19 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import MyContext from '../context/MyContext';
 import RecipeCard from '../components/RecipeCard';
-import CategoryMeal from '../components/CategoryMeal';
-import CategoryCocktail from '../components/CategoryCocktail';
 
 const MAX_LENGTH = 12;
 
 function Recipes() {
   const history = useHistory();
   const pathName = history.location.pathname;
-  const { search, defaultFood, defaultDrinks } = useContext(MyContext);
+  const {
+    search,
+    defaultFood,
+    defaultDrinks,
+    categories,
+    setCategory,
+    categoryButton,
+    setCategoryButton,
+  } = useContext(MyContext);
   const defaultResults = pathName.includes('foods') ? defaultFood : defaultDrinks;
+  // const categoryResults = !categoryButton[0] ? defaultResults : categoryButton;
+  // const toRenderDefault = !search.searchResult[0] ? defaultResults : renderResults;
   const renderResults = search.searchResult.slice(0, MAX_LENGTH);
   const obj = {
     title: pathName.includes('foods') ? 'Foods' : 'Drinks',
@@ -21,16 +29,62 @@ function Recipes() {
     database: pathName.includes('foods') ? 'meal' : 'cocktail',
     toRender: !search.searchResult[0] ? defaultResults : renderResults,
   };
+  const databaseTeste = pathName.includes('foods') ? 'meal' : 'cocktail';
+
+  async function getCategoryApi(categorySelected) {
+    const apiResponseButton = pathName.includes('foods') ? 'meals' : 'drinks';
+    const categoryResponse = await fetch(`https://www.the${databaseTeste}db.com/api/json/v1/1/filter.php?c=${categorySelected}`)
+      .then((response) => response.json());
+    setCategoryButton(categoryResponse[apiResponseButton]);
+    // console.log('categoryButn', categoryResponse);
+    // console.log(categorySelected);
+  }
+
+  useEffect(() => {
+    async function fetchCategories() {
+      const apiResponse = pathName.includes('foods') ? 'meals' : 'drinks';
+      const resultCategories = await fetch(`https://www.the${databaseTeste}db.com/api/json/v1/1/list.php?c=list`)
+        .then((response) => response.json());
+      setCategory(resultCategories[apiResponse]);
+      // console.log(resultCategories);
+      // console.log('resultCategories[apiResponse]', resultCategories[apiResponse]);
+    }
+    fetchCategories();
+  }, [obj.database]);
+
+  const MAX_CATEGORY = 5;
+  const MAX_RECIPES_CATEGORY = 12;
   return (
     <>
       { pathName.split('/').length === 2
-      && <Header title={ obj.title } />}
-      { obj.title === 'Foods' ? <CategoryMeal /> : <CategoryCocktail />}
+&& <Header title={ obj.title } />}
+      <button
+        type="button"
+        data-testid="All-category-filter"
+        onClick={ () => setCategoryButton([]) }
+      >
+        All
+      </button>
+      { categories && categories.slice(0, MAX_CATEGORY).map((categoriesBtn, index) => (
+        <button
+          type="button"
+          key={ index }
+          data-testid={ `${categoriesBtn.strCategory}-category-filter` }
+          onClick={ () => getCategoryApi(categoriesBtn.strCategory) }
+        >
+          {categoriesBtn.strCategory}
+        </button>))}
+      { categoryButton.slice(0, MAX_RECIPES_CATEGORY).map((category, index1) => (
+        <RecipeCard
+          key={ category[`id${obj.currentAPI}`] }
+          index={ index1 }
+          name={ category[`str${obj.currentAPI}`] }
+          image={ category[`str${obj.currentAPI}Thumb`] }
+        />
+      ))}
       {
         obj.toRender.map((recipe, index) => (
           <RecipeCard
-            path={ pathName }
-            id={ recipe[`id${obj.currentAPI}`] }
             key={ recipe[`id${obj.currentAPI}`] }
             index={ index }
             name={ recipe[`str${obj.currentAPI}`] }
