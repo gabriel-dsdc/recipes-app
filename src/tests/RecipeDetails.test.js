6c9c185fab1048cadf34000ce05702613386cff6
André Teixeira <andrevzp1@gmail.com>
2022-07-25 11:39:25 -0300

    Criando a primeira versão do arquivo de testes das telas de receitas

---
+++ src/tests/RecipeDetails.test.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './helpers/renderWithRouter';
+import {progress, fav} from './mocks/localStorage'
+
+import copy from 'clipboard-copy';
+
+jest.mock("clipboard-copy");
+
+describe('verifica tela de Progresso', () => {
+
+	test('verifica geral de pag progresso', async () => {
+
+		localStorage.clear();
+		localStorage.setItem('inProgressRecipes', JSON.stringify(progress));
+		const { history } = renderWithRouter(<App />, "/drinks/17222");
+		const titulo = await screen.findByRole('heading', { name: /a1/i }, {timeout: 3500});
+		expect(titulo).toBeInTheDocument();
+		const image = screen.getByTestId("recipe-photo")
+		expect(image).toBeInTheDocument();
+		const shareBtn = screen.getByTestId("share-btn")
+		expect(shareBtn).toBeInTheDocument();
+		const favBtn = screen.getByTestId("favorite-btn")
+		expect(favBtn).toBeInTheDocument();
+		const continueRecipe = screen.getByTestId("start-recipe-btn");
+		userEvent.click(continueRecipe);
+		const {pathname} = history.location;
+		expect(pathname).toBe("/drinks/17222/in-progress");
+
+
+	},6000)
+
+	test('verifica fav click', async () => {
+
+		localStorage.clear();
+		localStorage.setItem('inProgressRecipes', JSON.stringify(progress));
+		localStorage.setItem('favoriteRecipes', JSON.stringify(fav));
+		renderWithRouter(<App />, "/drinks/17222");
+		await screen.findByRole('heading', { name: /a1/i }, {timeout: 3500});
+		const btnFav = screen.getByTestId("favorite-btn");
+		userEvent.click(btnFav);
+		userEvent.click(btnFav);
+
+	},6000)
+
+	test('verifica btn compartilhar', async () => {
+		copy.mockImplementation(() => {})
+		localStorage.clear();
+		renderWithRouter(<App />, "/drinks/17222");
+		const btnShare = await screen.findAllByTestId(/share-btn/,'',{timeout:3500});
+		userEvent.click(btnShare[0]);
+		const messageCopy = screen.getAllByText(/link copied!/i);
+		expect(messageCopy[0]).toBeInTheDocument();
+		const btnFav = screen.getByTestId(/favorite-btn/i);
+		userEvent.click(btnFav);
+	}, 12000)
+});
